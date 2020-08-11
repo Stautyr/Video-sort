68435f454fd41227d413ca8570627fdec91b3293
Stautyr <47163082+Stautyr@users.noreply.github.com>
2020-08-10 23:42:26 -0500

    Update Video-Sort.py

--- Video-Sort.py
+++ Video-Sort.py
@@ -10,13 +10,14 @@ from pathlib import Path
 DEBUG = True
 
 def sort(fileName, ext):
-	x = [fileName[0:4],fileName[5:7], fileName[8:10]]
-	year = int(x[0])
-	month = int(x[1])
-	day = int(x[2])
-	date = datetime(year, month, day)
-	date.strftime(%B, %d) 
-	print (date)
+	if (ext == ".m4a" or ext == ".mp4"):
+		x = [fileName[0:4],fileName[5:7], fileName[8:10]]
+		year = int(x[0])
+		month = int(x[1])
+		day = int(x[2])
+		date = datetime(year, month, day)
+		date.strftime(%B, %d) 
+		print (date)
 
 start = "F:\\Gameplay\\VODS"
 os.chdir(start)
